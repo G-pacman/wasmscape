3652d8069b74a68e3efdd36c16e5ef6ea2a51697
Greg Paczkowski <paczk015@umn.edu>
2020-02-22 20:42:35 -0600

    did some more tests with the hello.c file

--- hello.c
+++ hello.c
@@ -3,6 +3,10 @@
 int main(int argc, char ** argv) {
   printf("Hello, world!\n");
   int i = 50;
+  char buf[50];
   //scanf("%d", &i);
-  printf("%d is your int\n", i);
+  printf("%d is your int\n\n", i);
+  fgets(buf, 50, stdin);
+  printf("another test print statement\n");
+  printf("%s\n",buf);
 }
